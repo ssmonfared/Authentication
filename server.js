2c91baa7fe191f1f4a837b777de0a5b9fd7702b8
Sahand Saniee Monfared <ssmonfared@ce.sharif.edu>
2021-05-05 11:53:23 -0500

    comment

--- server.js
+++ server.js
@@ -45,9 +45,6 @@ app.use(function(req, res, next) {
   if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
     jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function(err, decode) {
       if (err) req.user = undefined;
-      console.log("khuruji");
-      console.log(req.headers);
-      console.log(req.headers.authorization);
       req.user = decode;
       next();
     });
